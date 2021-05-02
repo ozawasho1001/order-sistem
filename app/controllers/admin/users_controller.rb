899f695184ff65b2ee62c8d18c06eef591aeb9e0
ozawasho1001 <sho@ozawashounoMacBook-Pro.local>
2021-05-03 00:27:28 +0900

    ユーザー側のヘッダーの修正をした。

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,7 +8,7 @@ class Admin::UsersController < Admin::BaseController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_topics_path, success: '登録が完了しました'
+      redirect_to admin_users_path, success: '登録が完了しました'
     else
       flash.now[:danger] = '登録失敗です'
       render :new
